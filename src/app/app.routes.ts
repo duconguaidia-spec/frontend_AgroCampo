3e73c224f0ca4fb2be7f3587c128ba11c5a0b1cf
duconguaidia-spec <duconguaidia@gmail.com>
2026-08-13 15:51:19 -0500

    Se creo el componente de registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './pages/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'register',
+        pathMatch: 'full'
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
+];
